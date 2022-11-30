4260933ea9a50e21c4a0cc52ed04f4d49e79ca7b
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2022-11-30 14:50:54 +0200

    adding my lab with equation

---
+++ lab1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    double y = 7.315 , z = 3.127;
+    double p1 =16*pow(y,2) ;
+    double p2 =exp(y*z) ;
+    double p3 = cbrt(z+1.51);
+    double p4 = log10(y*z);
+
+    double a=p1+ p2 +p3+p4;
+
+    printf("result: %lf\n", a);
+
+    return 0;
+}
